9a640e58a4fd2b67fbdc366b572b78b54b4b7fe6
MUI LE <tanmui1992@gmail.com>
2022-06-18 00:27:47 +0800

    update cors

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,30 +1,21 @@
-const url = 'https://api.yelp.com/v3/businesses/search?location=singapore&categories=sushi, All';
-const apiKey =
-  'XbwVX7w36FwIoJR-cLgnNHErUzWI0SBOAUJYoe0PTjpGuofzTpk0xDrogIA-zx4Q2cUClcg4AjVSe8o7khBxTumGTf5_co2YKzbgeHfGi9i9pbiL8zR6sqjZDJalYnYx';
+const urlSushi = 'https://api.yelp.com/v3/businesses/search?location=singapore&categories=sushi, All';
 
 async function fetchData(url) {
-  const response = await fetch(url, {
-    method: 'GET',
-    // headers: {
-    //   'Content-Type': 'application/json',
-    //   'x-requested-with': 'xmlhttprequest',
-    //   'Access-Control-Allow-Origin': '*',
-    //   Authorization:
-    //     'Bearer XbwVX7w36FwIoJR-cLgnNHErUzWI0SBOAUJYoe0PTjpGuofzTpk0xDrogIA-zx4Q2cUClcg4AjVSe8o7khBxTumGTf5_co2YKzbgeHfGi9i9pbiL8zR6sqjZDJalYnYx',
-    // },
-
-    headers: {
-      accept: 'application/json',
-      'x-requested-with': 'xmlhttprequest',
-      'Access-Control-Allow-Origin': '*',
-      Authorization: `Bearer ${apiKey}`,
-    },
-    data: {
-      term: 'Bar',
-      location: 'los angeles',
-    },
-  });
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        // 'Content-Type': 'application/json',
+        // 'x-requested-with': 'xmlhttprequest',
+        // 'Access-Control-Allow-Origin': '*',
+        Authorization:
+          'Bearer XbwVX7w36FwIoJR-cLgnNHErUzWI0SBOAUJYoe0PTjpGuofzTpk0xDrogIA-zx4Q2cUClcg4AjVSe8o7khBxTumGTf5_co2YKzbgeHfGi9i9pbiL8zR6sqjZDJalYnYx',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(`Hey man! This city is not exist in my planet. Please choose again! 👽`);
+  }
 }
-fetchData(url);
+fetchData('https://cors-anywhere.herokuapp.com/' + urlSushi);
