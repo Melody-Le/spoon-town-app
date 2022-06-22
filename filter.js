09c469a751c80db8b3d98d137fc718d52f305e35
MUI LE <tanmui1992@gmail.com>
2022-06-22 22:54:57 +0800

    set defaut value when user not allow access location

--- filter.js
+++ filter.js
@@ -1,13 +1,20 @@
 const btnFilter = document.querySelector('.btn-filter');
 const filterContainer = document.querySelector('.search-filter-box');
-const filterCatContainer = document.querySelector('.filter-catogery-container');
+const filterCategoryContainer = document.querySelector(
+  '.filter-catogery-container'
+);
 const searchPlaceContainer = document.querySelector('.search-option-container');
 const restaurantCardContainer = document.querySelector('.container-card');
+const errorContainer = document.querySelector('.error-container');
+
+//STATE:
+let selectedPlace;
+const selectedCategories = [];
 
 class RestaurantFilter {
   constructor(data) {
     const address = Object.values(data.location).toString();
-    this.id = data.id;
+    const title = data.categories.map(item => item.title).join(', ');
     this.restaurantName = data.name;
     this.image = data.image_url;
     this.sourceUrl = data.url;
@@ -15,6 +22,7 @@ class RestaurantFilter {
     this.price = data.price;
     this.phone = data.phone;
     this.address = address;
+    this.title = title;
   }
   showRestaurantCard(parentElm) {
     const html = `
@@ -28,6 +36,7 @@ class RestaurantFilter {
           />
           <h5 class="card-title restaurant-name">${this.restaurantName}</h5>
           <p class="card-text cotent">Address: ${this.address}</p>
+          <p class="card-text cotent">Search Title: ${this.title}</p>
           <p class="card-text review"> Review: ${this.rating}</p>
           
         </div>
@@ -58,27 +67,34 @@ class topPickPlace {
     return coords;
   }
 }
-//get currentLocation :
+
+const renderError = () => {
+  const html = `<p class = "text-center p-lg-4 bg-warning" id = "error">Please allow access your location to have better suggestion restaurant. <br> In the meantime, we will set your current Location is in Singapore </p>`;
+  errorContainer.insertAdjacentHTML('beforeend', html);
+  setTimeout(() => {
+    document.querySelector('#error').textContent = '';
+  }, 3000);
+};
+
 const getLocation = () =>
   new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        const coords = [latitude, longitude];
-        resolve(coords);
-      },
-      error => {
-        console.log('Could not get your location, we will set location in Singapore');
-        reject(error);
-      }
-    );
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+      resolve(coords);
+    }, error());
   });
 
-async function getCurrentLocaiton() {
-  const location = await getLocation();
-  console.log(location);
-  return location;
-}
+const getCurrentLocaiton = async function () {
+  try {
+    const location = await getLocation();
+    return location;
+  } catch {
+    renderError();
+    const location = [1.2925005, 103.8547508];
+    return location;
+  }
+};
 
 async function callApi(url) {
   const cors = 'https://melodycors.herokuapp.com/';
@@ -94,8 +110,6 @@ async function callApi(url) {
   return data;
 }
 
-//list of topPick Place:
-// const nearBy =
 const orchard = new topPickPlace('Ion Orchard', 1.304052, 103.831764);
 const sentosa = new topPickPlace('Sentosa, Singapore', 1.249404, 103.830322);
 const novena = new topPickPlace('Novena', 1.3203434, 103.8406453);
@@ -103,55 +117,43 @@ const hougang = new topPickPlace('Hougang', 1.3725948, 103.8915338);
 
 const placeList = [orchard, sentosa, novena, hougang];
 
-//show topPick list in filter box:
-placeList.forEach(item => {
-  item.showTopPickPlace(searchPlaceContainer);
-});
-
-// When user click topPick list => get the place name => get the Lat & long => put inside the APIlink
-const selectedPlaceLocation = [];
-let selectedLocation;
-const getPlace = function () {
-  searchPlaceContainer.addEventListener('click', async e => {
-    console.log(e.target.classList);
-    if (!e.target.classList.contains('location-name')) return;
-    if (e.target.classList.contains('near-by')) {
-      console.log('Near By');
-      const nearByLocation = await getCurrentLocaiton();
-      // getRestaurantObjListByLocation(nearByLocation);
-      // getCategories(nearByLocation);
-      selectedLocation = nearByLocation;
-      showCategoriesList(nearByLocation);
-      // userAddCategories(nearByLocation);
-      // renderFilterPage();
-      return;
-    }
-    const placeName = e.target.textContent.toLowerCase();
-    const selectedPlaceLowercase = placeList.find(place => place.locationName.toLowerCase() === placeName);
-    const topPickLocation = selectedPlaceLowercase?.getPosition();
-    // getRestaurantObjListByLocation(location);
-    console.log(topPickLocation);
-    selectedLocation = topPickLocation;
-    // getRestaurantObjListByLocation(topPickLocation);
-    // getCategories(topPickLocation);
-    showCategoriesList(topPickLocation);
-    // userAddCategories(topPickLocation);
-    // renderFilterPage();
+const renderTopPickSearch = () => {
+  placeList.forEach(item => {
+    item.showTopPickPlace(searchPlaceContainer);
   });
 };
 
-async function getRestaurantObjListByLocation(location) {
-  // const [lat, long] = await getLocation();
-  // console.log(`https://www.google.com/maps/@${lat},${long}`);
+const userSelectPlace = () => {
+  selectedPlace = '';
+  searchPlaceContainer.addEventListener('click', async e => {
+    if (!e.target.classList.contains('location-name')) return;
+    if (e.target.classList.contains('near-by')) {
+      const nearByLocation = await getCurrentLocaiton();
+      renderCategoriesList(nearByLocation);
+      selectedPlace = nearByLocation;
+      return;
+    }
+    const userSelectPlace = e.target.textContent.toLowerCase();
+    const getSelectedPlaceObj = placeList.find(
+      place => place.locationName.toLowerCase() === userSelectPlace
+    );
+    const topPickLocation = getSelectedPlaceObj.getPosition();
+    selectedPlace = topPickLocation;
+    renderCategoriesList(topPickLocation);
+  });
+};
+
+const getRestaurantObjListByLocation = async function (location) {
   const [lat, long] = location;
   const urlRestaurant = `https://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=${lat}&longitude=${long}`;
   const restaurantAPI = await callApi(urlRestaurant);
   const { businesses: restaurantObjList } = restaurantAPI;
   return restaurantObjList;
-}
+};
+
 //NOTE: CATOGERIES:
 
-async function getCategories(location) {
+const getCategories = async function (location) {
   const restaurantObjList = await getRestaurantObjListByLocation(location);
   const restaurantCategories = restaurantObjList
     .map(obj => obj.categories)
@@ -159,9 +161,9 @@ async function getCategories(location) {
     .map(item => item.title);
   const catogeries = [...new Set(restaurantCategories)];
   return catogeries;
-}
+};
 
-async function showCategoriesList(location) {
+const renderCategoriesList = async function (location) {
   const categoriesList = await getCategories(location);
   categoriesList.forEach(categoryItem => {
     const filterHtml = `
@@ -170,50 +172,51 @@ async function showCategoriesList(location) {
           ${categoryItem}
         </a>
       </li>`;
-    filterCatContainer.insertAdjacentHTML('beforeend', filterHtml);
+    filterCategoryContainer.insertAdjacentHTML('beforeend', filterHtml);
   });
-}
-const selectedCatList = [];
-function selectCategoriesList(e, location) {
-  const catSelect = e.target;
-  if (!catSelect.classList.contains('search-categories')) return;
-  const catContent = catSelect?.textContent;
-  selectedCatList.push(catContent.trim());
-  // getRestaurantFilterLink(location, selectedCatList);
-  renderFilterPage(selectedCatList);
-}
-function userAddCategories(location) {
-  filterCatContainer.addEventListener('click', selectCategoriesList);
-}
+};
 
-async function getRestaurantFilterLink(location, catLink) {
-  // const [lat, long] = await getLocation();
+// Render Page:
+const getFilterLink = async function (location, selectedCatogeriesList) {
   const [lat, long] = location;
-  const selectedCatLink = catLink?.reduce((acc, cur) => `${acc}&categories=${cur}`);
-  const updatedCatLink = `&categories=${selectedCatLink}`;
-  const urlFilterLink = `https://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=${lat}&longitude=${long}${updatedCatLink}`;
+  console.log(location);
+  const selectedCategorytLink = selectedCatogeriesList?.reduce(
+    (acc, cur) => `${acc}&categories=${cur}`
+  );
+  const updatedCategoryLink = `&categories=${selectedCategorytLink}`;
+  const urlFilterLink = `https://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=${lat}&longitude=${long}${updatedCategoryLink}`;
   return urlFilterLink;
-}
+};
 
-async function renderFilterPage(link) {
-  const url = await getRestaurantFilterLink(selectedLocation, link);
+const renderFilterPage = async function (filterLink) {
+  const url = await getFilterLink(selectedPlace, filterLink);
+  console.log(url);
   const resulf = await callApi(url);
   const { businesses: data } = resulf;
-  console.log(data);
   data.forEach(resObj => {
     const restaurantCard = new RestaurantFilter(resObj);
     restaurantCard.showRestaurantCard(restaurantCardContainer);
   });
-}
+};
 
-function init() {
-  // step 1
-  getPlace();
-  // userSelectPlace();
-  // showCategoriesList();
+const userSelecCategoryList = e => {
+  if (!e.target.classList.contains('search-categories')) return;
+  const catogeriesContent = e.target.textContent;
+  selectedCategories.push(catogeriesContent.trim());
+  console.log(selectedCategories);
+  renderFilterPage(selectedCategories);
+};
+
+const userAddCategories = () => {
+  filterCategoryContainer.addEventListener('click', userSelecCategoryList);
+};
+
+const init = function () {
+  // step 1: Window Load:
+  renderTopPickSearch();
   // step 2
-  userAddCategories();
-  // renderFilterPage();
+  userSelectPlace();
   // step 3
-}
+  userAddCategories();
+};
 init();
