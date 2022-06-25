e1a6eeeeebd2f8c3952c0e0347ca51e2be72e2a6
MUI LE <tanmui1992@gmail.com>
2022-06-25 12:10:01 +0800

    show ToppickLocation when page filter load

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,5 @@
 // proxy server
-const callApi = async (url) => {
+const callApi = async url => {
   const cors = 'https://melodycors.herokuapp.com/';
   const apiKey =
     'XbwVX7w36FwIoJR-cLgnNHErUzWI0SBOAUJYoe0PTjpGuofzTpk0xDrogIA-zx4Q2cUClcg4AjVSe8o7khBxTumGTf5_co2YKzbgeHfGi9i9pbiL8zR6sqjZDJalYnYx';
@@ -10,28 +10,63 @@ const callApi = async (url) => {
     },
   });
   return await response.json();
+};
+class topPickPlace {
+  constructor(locationName, latitude, longitude) {
+    this.locationName = locationName;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+  showTopPickPlace(parentElm) {
+    const htmlTopPick = `
+      <li class="search-option-item">
+      <a href="#" class = " search-location search-top-pick">
+        <img class="icon" src="./img/map-point-icon.svg" alt="icon Nearby">
+        <p class = "location-name">${this.locationName}</p>
+      </a>
+      </li>`;
+    parentElm.insertAdjacentHTML('beforeend', htmlTopPick);
+  }
+  getPosition() {
+    const coords = [this.latitude, this.longitude];
+    return coords;
+  }
 }
+
+const showTopPickLocation = () => {
+  const searchPlaceContainer = document.querySelector(
+    '.search-option-container'
+  );
+  const orchard = new topPickPlace('Ion Orchard', 1.304052, 103.831764);
+  const sentosa = new topPickPlace('Sentosa, Singapore', 1.249404, 103.830322);
+  const novena = new topPickPlace('Novena', 1.3203434, 103.8406453);
+  const hougang = new topPickPlace('Hougang', 1.3725948, 103.8915338);
+  const placeList = [orchard, sentosa, novena, hougang];
+  placeList.forEach(place => {
+    place.showTopPickPlace(searchPlaceContainer);
+  });
+};
 
 const getUserCurrentPosition = () => {
   return new Promise((resolved, rejected) =>
     navigator.geolocation.getCurrentPosition(resolved, rejected)
   );
-}
+};
 
 const getCurrentLocation = async () => {
   try {
     const position = await getUserCurrentPosition();
     const { latitude, longitude } = position.coords;
     return { latitude, longitude };
-  } catch(ex) {
+  } catch (ex) {
     return {
       latitude: 1.2925005,
-      longitude: 103.8547508
-    }
+      longitude: 103.8547508,
+    };
   }
 };
 
-const getRestaurantsByLocation = async (location) => {
+const getRestaurantsByLocation = async location => {
   const { latitude, longitude } = location;
   const urlRestaurant = `https://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=${latitude}&longitude=${longitude}`;
   const restaurantApi = await callApi(urlRestaurant);
@@ -39,7 +74,7 @@ const getRestaurantsByLocation = async (location) => {
   return businesses;
 };
 
-const getCategoriesByLocation = async (location) => {
+const getCategoriesByLocation = async location => {
   const restaurantObjList = await getRestaurantsByLocation(location);
   const restaurantCategories = restaurantObjList
     .map(obj => obj.categories.map(category => category.title))
@@ -47,8 +82,10 @@ const getCategoriesByLocation = async (location) => {
   return [...new Set(restaurantCategories)];
 };
 
-const showCategories = (categories) => {
-  const filterCategoryContainer = document.querySelector('.filter-catogery-container');
+const showCategories = categories => {
+  const filterCategoryContainer = document.querySelector(
+    '.filter-catogery-container'
+  );
   categories.forEach(category => {
     const filterHtml = `
       <li class="search-option-item">
@@ -65,22 +102,25 @@ const onNearByClicked = async () => {
   const currentUserLocation = await getCurrentLocation();
   const categories = await getCategoriesByLocation(currentUserLocation);
   showCategories(categories);
-}
+};
 
 const addEventListeners = () => {
-  document.querySelector('.search-option-container').addEventListener('click', (evnt) => {
-    const target = evnt.target;
-    target.style.color = 'Red';
+  document
+    .querySelector('.search-option-container')
+    .addEventListener('click', evnt => {
+      const target = evnt.target;
+      target.style.color = 'Red';
 
-    if (!target.classList.contains('location-name')) return;
-    if (target.classList.contains('near-by')) {
-      onNearByClicked();
-      return;
-    }
-  })
-}
+      if (!target.classList.contains('location-name')) return;
+      if (target.classList.contains('near-by')) {
+        onNearByClicked();
+        return;
+      }
+    });
+};
 
 const init = () => {
-  addEventListeners()
+  showTopPickLocation();
+  addEventListeners();
 };
 init();
