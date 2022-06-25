9975a41668971477252507610aade01bd0546dd0
MUI LE <tanmui1992@gmail.com>
2022-06-25 13:30:39 +0800

    add categories as per Topick location

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,5 @@
 // proxy server
-const callApi = async url => {
+const callApi = async (url) => {
   const cors = 'https://melodycors.herokuapp.com/';
   const apiKey =
     'XbwVX7w36FwIoJR-cLgnNHErUzWI0SBOAUJYoe0PTjpGuofzTpk0xDrogIA-zx4Q2cUClcg4AjVSe8o7khBxTumGTf5_co2YKzbgeHfGi9i9pbiL8zR6sqjZDJalYnYx';
@@ -28,23 +28,42 @@ class topPickPlace {
     parentElm.insertAdjacentHTML('beforeend', htmlTopPick);
   }
   getPosition() {
-    const coords = [this.latitude, this.longitude];
-    return coords;
+    return {
+      latitude: this.latitude,
+      longitude: this.longitude,
+    };
   }
 }
+
+const getTopPickPlaces = () => {
+  const orchard = new topPickPlace('Ion Orchard', 1.304052, 103.831764);
+  const sentosa = new topPickPlace('Sentosa, Singapore', 1.249404, 103.830322);
+  const novena = new topPickPlace('Novena', 1.3203434, 103.8406453);
+  const hougang = new topPickPlace('Hougang', 1.3725948, 103.8915338);
+  console.log(orchard);
+  const placeList = [orchard, sentosa, novena, hougang];
+  return placeList;
+};
 
 const showTopPickLocation = () => {
   const searchPlaceContainer = document.querySelector(
     '.search-option-container'
   );
-  const orchard = new topPickPlace('Ion Orchard', 1.304052, 103.831764);
-  const sentosa = new topPickPlace('Sentosa, Singapore', 1.249404, 103.830322);
-  const novena = new topPickPlace('Novena', 1.3203434, 103.8406453);
-  const hougang = new topPickPlace('Hougang', 1.3725948, 103.8915338);
-  const placeList = [orchard, sentosa, novena, hougang];
-  placeList.forEach(place => {
+  const placeList = getTopPickPlaces();
+  placeList.forEach((place) => {
     place.showTopPickPlace(searchPlaceContainer);
   });
+};
+
+const getTopLickLocation = async (evnt) => {
+  const placeList = getTopPickPlaces(); //FIXME: I WANNA GET THE PLACELIST IN showTopPickLocation() funciton, but if I call it here, It will have error
+  const target = evnt.target;
+  const userSelectPlace = target.textContent.toLowerCase();
+  const getSelectedPlaceObj = placeList.find(
+    (place) => place.locationName.toLowerCase() === userSelectPlace
+  );
+  console.log(getSelectedPlaceObj.getPosition());
+  return getSelectedPlaceObj.getPosition();
 };
 
 const getUserCurrentPosition = () => {
@@ -66,7 +85,7 @@ const getCurrentLocation = async () => {
   }
 };
 
-const getRestaurantsByLocation = async location => {
+const getRestaurantsByLocation = async (location) => {
   const { latitude, longitude } = location;
   const urlRestaurant = `https://api.yelp.com/v3/businesses/search?categories=restaurants&latitude=${latitude}&longitude=${longitude}`;
   const restaurantApi = await callApi(urlRestaurant);
@@ -74,19 +93,19 @@ const getRestaurantsByLocation = async location => {
   return businesses;
 };
 
-const getCategoriesByLocation = async location => {
+const getCategoriesByLocation = async (location) => {
   const restaurantObjList = await getRestaurantsByLocation(location);
   const restaurantCategories = restaurantObjList
-    .map(obj => obj.categories.map(category => category.title))
+    .map((obj) => obj.categories.map((category) => category.title))
     .flat();
   return [...new Set(restaurantCategories)];
 };
 
-const showCategories = categories => {
+const showCategories = (categories) => {
   const filterCategoryContainer = document.querySelector(
     '.filter-catogery-container'
   );
-  categories.forEach(category => {
+  categories.forEach((category) => {
     const filterHtml = `
       <li class="search-option-item">
         <a href="#" class="search-categories">
@@ -104,10 +123,16 @@ const onNearByClicked = async () => {
   showCategories(categories);
 };
 
+const onTopPickClicked = async (evnt) => {
+  const selectedTopPickLocation = await getTopLickLocation(evnt);
+  const categories = await getCategoriesByLocation(selectedTopPickLocation);
+  showCategories(categories);
+};
+
 const addEventListeners = () => {
   document
     .querySelector('.search-option-container')
-    .addEventListener('click', evnt => {
+    .addEventListener('click', (evnt) => {
       const target = evnt.target;
       target.style.color = 'Red';
 
@@ -116,6 +141,7 @@ const addEventListeners = () => {
         onNearByClicked();
         return;
       }
+      onTopPickClicked(evnt);
     });
 };
 
