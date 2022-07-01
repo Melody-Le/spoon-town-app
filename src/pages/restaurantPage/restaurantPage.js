47a86a7c8a902c6b8c218670d323a33515e7fcd3
MUI LE <tanmui1992@gmail.com>
2022-07-01 17:36:14 +0800

    done css for restaurant detail

--- src/pages/restaurantPage/restaurantPage.js
+++ src/pages/restaurantPage/restaurantPage.js
@@ -51,9 +51,6 @@ class Review {
             <p class="user-comment">${this.#comment}</p>
             <p class="comment-time">review time: ${this.#commentTime}</p>
             <li class="userProfile">
-              <a href="${this.#reviewUrl}">Click here for more Review</a>
-            </li>
-            <li class="userProfile">
               <a href="${this.#userProfileUrl}">User Profile</a>
             </li>
           </div>
@@ -75,16 +72,18 @@ class Restaurant {
   #phone;
   #address;
   #cuisine;
-  #isClose;
-  #isOpen;
   #reviewCount;
   #latitude;
   #longitude;
+  #isOpen;
+  #isOpenNow;
   constructor(restaurantApi) {
     const address = Object.values(restaurantApi.location.display_address).join(", ");
     const title = restaurantApi.categories.map((item) => item.title).join(", ");
     const [photo1, photo2, photo3] = restaurantApi.photos;
     const { latitude, longitude } = restaurantApi.coordinates;
+    const { is_open_now } = restaurantApi.hours[0];
+
     this.#idRestaurantApi = restaurantApi.id;
     this.#restaurantName = restaurantApi.name;
     this.#photo1 = photo1;
@@ -95,22 +94,25 @@ class Restaurant {
     this.#phone = restaurantApi.phone;
     this.#address = address;
     this.#cuisine = title;
-    this.#isClose = restaurantApi.is_closed;
-    this.#isOpen = this.#isClose ? "Closed" : "Open now";
     this.#reviewCount = restaurantApi.review_count;
     this.#latitude = latitude;
     this.#longitude = longitude;
+    this.#isOpen = is_open_now;
+    this.#isOpenNow = this.#isOpen ? "Open now" : "Closed";
   }
 
   showRestaurantCard(parentElm) {
     const starPercentage = `${(this.#rating / 5) * 100}%`;
     const html = `
-      <div class="my-3">
-        <h1 class="restaurant-name">${this.#restaurantName}</h1>
-        <div class="user-rating">
-          <div class="stars-outer">
-            <div class="stars-inner" style="width:${starPercentage}"></div>
+      <div class="my-4">
+        <h2 class="restaurant-name">${this.#restaurantName}</h2>
+        <div class="restaurant-review-information">
+          <div class="user-rating">
+            <div class="stars-outer">
+              <div class="stars-inner" style="width:${starPercentage}"></div>
+            </div>
           </div>
+          <h6>${this.#reviewCount} review</h6>
         </div>
       </div>
       
@@ -125,26 +127,34 @@ class Restaurant {
           <img src="${this.#photo3}" alt="restaurant-image-${this.#idRestaurantApi}" />
         </figure>
       </div>
-
+      <h3 class="mt-4">Detail</h3>
       <div class="row gy-3 gx-2">
         <div class="detail-restaurant col-lg-6">
-          <h3>Detail</h3>
-          <p>Adress: ${this.#address}</p>
-          <p>Phone: ${this.#phone}</p>
-          <p>Cuisine: ${this.#cuisine}</p>
-          <p>${this.#isOpen}</p>
-          <p>Read all ${this.#reviewCount} review</p>
+          <div class="information">
+            <img class="location-icon detail-icon" src="/src/img/icon-location-orange.png" alt="icon-location">
+            <p class="my-auto">${this.#address}</p>
+          </div>
+          <div class="information">
+            <img class="phone-icon detail-icon" src="/src/img/phone-icon.png" alt="icon-location">
+            <p class="my-auto">${this.#phone}</p>
+          </div>
+          <div class="information">
+            <img class="cuisine-icon detail-icon" src="/src/img/favicon.png" alt="icon-location">
+            <p class="my-auto">Cuisine: ${this.#cuisine}</p>
+          </div>
+          <div class="information">
+            <img class="open-now-icon detail-icon" src="/src/img/open-now-icon.png" alt="icon-location">
+            <p class="my-auto">${this.#isOpenNow}</p>
+          </div>
           <div class="other-button">
-            <li class="btn p-2">
+            <li class="btn btn-reservation">
               <a class="page-url" href="${this.#sourceUrl}">MAKE A RESERVATION</a>
             </li>
-            <button class="btn p-2">SHARE</button>
-            <button class="btn p-2">♥️</button>
-            <button class="btn p-2">📌</button>
+            <button class="btn btn-share"><i class="fas fa-share"></i></button>
+            <button class="btn btn-like">&hearts;</button>
           </div>
         </div>
         <div class="map-location col-lg-6" id="map">
-          I AM API MAP TO SHOW LOCATION OF THIS RESTAURANT.
         </div>
       </div>
     `;
@@ -152,7 +162,7 @@ class Restaurant {
   }
 
   showMap() {
-    const map = L.map("map").setView([this.#latitude, this.#longitude], 15);
+    const map = L.map("map").setView([this.#latitude, this.#longitude], 17);
 
     L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -190,6 +200,10 @@ const addEventListener = () => {
     const oldActive = document.querySelector(".image-active");
     oldActive.classList.remove("image-active");
     target.parentElement.classList.add("image-active");
+  });
+  document.querySelector(".btn-like").addEventListener("click", (evnt) => {
+    const target = evnt.target;
+    target.classList.toggle("like");
   });
 };
 
